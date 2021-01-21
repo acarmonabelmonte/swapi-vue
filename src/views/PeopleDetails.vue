a1a3a7df3d209e0e5a0aaaea73ebda34f3810288
Adrián Carmona <acarmona.dev@gmail.com>
2021-01-21 12:03:13 +0100

    finished first version of project :rocket:

--- src/views/PeopleDetails.vue
+++ src/views/PeopleDetails.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
     <PageBanner :title="peopleDetails.name" />
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap -m-4 text-center">
-        <div
-          class="p-4 sm:w-1/2 w-full"
-          v-for="(key, index) in Object.keys(peopleDetails)"
-          :key="index"
-        >
-          <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">
-            {{ peopleDetails[key] }}
-          </h2>
-          <p class="leading-relaxed uppercase">{{ key.replace("_", " ") }}</p>
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -m-4 mb-4">
+          <div
+            class="p-4 w-full lg:w-1/3"
+            v-for="(key, index) in Object.keys(peopleDetails).slice(0, 8)"
+            :key="index"
+          >
+            <div
+              class="h-full flex justify-center bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+            >
+              <Vue-Spinner
+                :loading="isLoading"
+                :color="color"
+                :size="size"
+              ></Vue-Spinner>
+              <div v-if="!isLoading">
+                <h1
+                  class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
+                >
+                  {{ peopleDetails[key] }}
+                </h1>
+                <p class="leading-relaxed">
+                  {{ key.replace("_", " ") }}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+        <GoBackLink />
       </div>
-      <GoBackLink />
-    </div>
+    </section>
   </div>
 </template>
 
@@ -23,18 +40,24 @@
 import { mapState } from "vuex";
 import GoBackLink from "@/components/UI/GoBackLink.vue";
 import PageBanner from "@/components/UI/PageBanner.vue";
+import VueSpinner from "@/components/UI/VueSpinner.vue";
 export default {
   data() {
     return {
       peopleId: this.$route.params.id,
+      color: "#4E4FE7",
+      size: "40px",
     };
   },
-  components: { GoBackLink, PageBanner },
+  components: { GoBackLink, PageBanner, VueSpinner },
   mounted() {
     this.$store.dispatch("people/fetchPeopleDetails", this.peopleId);
   },
   computed: mapState({
     peopleDetails: (state) => state.people.peopleDetails,
+    isLoading() {
+      return this.$store.getters.loadingStatus;
+    },
   }),
 };
 </script>
