00e756c41d82df3d3709e00297a26f784d643989
Adrián Carmona <acarmona.dev@gmail.com>
2021-01-21 12:22:03 +0100

    refactoring code

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div id="app">
-    <h1></h1>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/people">People</router-link> |
-      <router-link to="/planets">Planets</router-link> |
-      <router-link to="/starships">Starships</router-link> |
-    </div> -->
     <router-view />
     <MainFooter scrollTopPixels="200" />
   </div>
